74dc64fb90dadae2e458aafbc5c82bb4ce07a785
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2022-05-24 23:24:17 -1000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import placeholder from "./assets/images/ball.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main className="container">
+      <section className="link-container">
+        <a className="a">
+          <h1 className="h1 link">work</h1>
         </a>
-      </header>
-    </div>
+        <a className="a">
+          <h1 className="h1 link">"music"</h1>
+        </a>
+        <a className="a">
+          <h1 className="h1 link">contact</h1>
+        </a>
+      </section>
+      <section className="logo-container">
+        <h3 className="h3 logo">Gabe Fontanilla</h3>
+        <p className="p description">Frontend Developer</p>
+      </section>
+      <img className="img" src={placeholder} />
+    </main>
   );
 }
 
