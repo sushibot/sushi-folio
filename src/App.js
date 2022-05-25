980cf10b45988ccc41ce2b8a89b312ec0fc65b13
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2022-05-24 23:58:41 -1000

    feat: add logos and adjust placeholder img

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import "./App.css";
+import soundcloud from "./assets/images/soundcloud.png";
+import linkedin from "./assets/images/linkedin.png";
+import github from "./assets/images/github.png";
 import placeholder from "./assets/images/ball.png";
 function App() {
   return (
@@ -15,9 +18,20 @@ function App() {
             <h1 className="h1 link">contact</h1>
           </a>
         </section>
-        <section className="logo-container">
-          <h3 className="h3 logo">Gabe Fontanilla</h3>
+        <section className="name-logo-container">
+          <h3 className="h3 name">Gabe Fontanilla</h3>
           <p className="p description">Frontend Developer</p>
+          <div className="logo-container">
+            <a className="a">
+              <img className="logo-img" src={soundcloud} />
+            </a>
+            <a className="a">
+              <img className="logo-img" src={linkedin} />
+            </a>
+            <a className="a">
+              <img className="logo-img" src={github} />
+            </a>
+          </div>
         </section>
       </div>
       <img className="img" src={placeholder} />
